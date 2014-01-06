8d12c2869b52cdaabf8be16fea197df5b7ad11ca
Chaobin,Zhang <zhchbin@gmail.com>
2014-01-06 20:06:58 +0800

    [LeetCode] Palindrome Number.

---
+++ algorithm/leetcode/palindrome-number.cc
@@ -0,0 +1,27 @@
+// URL: http://oj.leetcode.com/problems/palindrome-number/
+//
+// Problem: Determine whether an integer is a palindrome. Do this without extra
+//          space.
+
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if (x < 0)
+      return false;
+
+    int div = 1;
+    while (x / div >= 10)
+      div *= 10;
+    while (x != 0) {
+      int l = x / div;
+      int r = x % 10;
+      if (l != r)
+        return false;
+
+      x = (x % div) / 10;
+      div /= 100;
+    }
+
+    return true;
+  }
+};
